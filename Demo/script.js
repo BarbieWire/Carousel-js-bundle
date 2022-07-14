9cb85495d13e22171518d55d77db0417a1db2b9b
barbie <kirillbondarenko365@gmail.com>
2022-07-15 01:12:00 +0300

    initial commit

---
+++ Demo/script.js
@@ -0,0 +1,5 @@
+const elem = document.querySelector("#carousel")
+const instance = new Carousel(elem, {initialSlide: 1})  // usage example via instance
+
+// pass the html5 object into new Carousel instance,
+// to create a Carousel wrapper above your object
